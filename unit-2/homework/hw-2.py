b039a0aca68c913bdcf47789afa9e62b97ff20c1
Hamid Pasha <hamidsinisa@yahoo.com>
2020-10-25 20:56:30 -0400

    upload to github

---
+++ unit-2/homework/hw-2.py
@@ -0,0 +1,15 @@
+worker_type= ['full_time', 'part_time', 'contract']
+hours_worked = 0
+hourly_pay = 0
+weekly_wage = hourly_worked * hourly_worked
+
+If worker_type = full_time:
+    hours_worked ==  50,
+    hourly_pay == 50
+elif
+    worker_type = part_time:
+    hours_worked ==  50,
+    hourly_pay == 65,
+elif 
+    worker_type = contract:
+    hourly_pay == 120-(120/4)
